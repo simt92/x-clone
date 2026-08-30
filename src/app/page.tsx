d6f4823a181ef034c32ddaa593c34a459bc30aa2
simt92 <240724700+simt92@users.noreply.github.com>
2026-08-30 16:53:34 +0900

    fix-page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,9 @@ import Home from "@/components/Home";
 export default async function Page() {
   const session = await auth();
 
-  const currentUserId = session?.user?.id ?? null;
+  const currentUserId = session?.user?.id
+    ? Number(session.user.id)
+    : null;
 
   return (
     <>
